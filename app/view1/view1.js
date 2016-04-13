843ff02eb1d677bb773038284a6c585aa991ef93
Mike Lambert <lambert8@illinois.edu>
2016-04-13 17:10:26 -0500

    'working' terminal?

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,22 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', [ '$scope', function($scope) {
+	
+	$scope.$on('terminal-input', function (e, consoleInput) {
+        var cmd = consoleInput[0];
+        console.log("Sending cmd: " + cmd);
+	});
+
+	$scope.send = function() {
+		$scope.$broadcast('terminal-output', {
+		    output: true,
+		    text: ['Welcome to vtortola.GitHub.io',
+		           'This is an example of ng-terminal-emulator.',
+		           '',
+		           "Please type 'help' to open a list of commands"],
+		    breakLine: true
+		});
+	};
 
 }]);
